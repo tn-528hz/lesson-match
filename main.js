89c64d76fb73ce694b13d3fbf342f5890b017c5d
Takato Noguchi <0ak792332w3227j@au.com>
2021-08-07 15:39:03 +0900

    first deploy

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ const expressValidator = require("express-validator");
 const connectFlash = require("connect-flash");
 const User = require("./models/user");
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://NoguchiTakato:hthmm0528T@cluster1.ognka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 mongoose.connect(
     //データベース接続を設定する
     process.env.MONGODB_URL || "mongodb://localhost:27017/user_db",
